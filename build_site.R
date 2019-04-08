a2986da794a56ef7d0e5b20b16071bf276dce3ff
Furqs92 <furqan@student.ie.edu>
2019-04-08 15:29:20 +0200

    Recommendation Engine

---
+++ build_site.R
@@ -0,0 +1,8 @@
+#Set our working directory. 
+#This helps avoid confusion if our working directory is 
+#not our site because of other projects we were 
+#working on at the time. 
+setwd("/Users/furqan/Documents/Madrid/IE/SecondTerm/RecommendationEngine/Assignment3/MuhammadFurqan/personal-site")
+
+#render your sweet site. 
+rmarkdown::render_site()
