0f66cb0dd87c5536fbb88fdc41a7c16b92d09648
Stavros Tsentemeidis <s.tsentemeidis@student.ie.edu>
2019-06-20 01:44:33 +0200

    Further Changes

--- build_site.R
+++ build_site.R
@@ -2,7 +2,9 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-setwd("/Users/stavrostsentemeidis/Documents/GitHub/personal-site/")
+setwd("/Users/stavrostsentemeidis/Documents/GitHub/personal-site")
 
 #render your sweet site. 
 rmarkdown::render_site()
+
+
