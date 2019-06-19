984534ba694e87b77bf48c91c0427d14fe0f4339
Stavros Tsentemeidis <s.tsentemeidis@student.ie.edu>
2019-06-19 14:57:55 +0200

    Customizing the demo version

--- build_site.R
+++ build_site.R
@@ -2,7 +2,7 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-setwd("/Users/furqan/Documents/Madrid/IE/SecondTerm/RecommendationEngine/Assignment3/MuhammadFurqan/personal-site")
+setwd("/Users/stavrostsentemeidis/Documents/GitHub/personal-site/")
 
 #render your sweet site. 
 rmarkdown::render_site()
